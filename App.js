dd2aa3828e82919db1e66189b9c5cc87900d80eb
sofau <sophiefauquembergue@gmail.com>
2024-11-28 15:26:30 +0100

    add custom actions

--- App.js
+++ App.js
@@ -1,40 +1,41 @@
-// import react Navigation
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-// Create the navigator
-const Stack = createStackNavigator();
-
 // import the screens
 import Chat from "./components/Chat";
 import Start from "./components/Start";
 
-// Import useNetInfo
-import { useNetInfo } from "@react-native-community/netinfo";
-import { useEffect } from "react";
+// import react Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import { LogBox, Alert } from "react-native";
-LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
-
-// import Firebase
+// import functions for initializing firestore
 import { initializeApp } from "firebase/app";
 import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { useNetInfo } from "@react-native-community/netinfo";
+import { useEffect } from "react";
+import { LogBox, Alert } from "react-native";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCVw3Pwp3eETBRHUFpdSnuE9g-F0D7oTg0",
-  authDomain: "chatme-62d79.firebaseapp.com",
-  projectId: "chatme-62d79",
-  storageBucket: "chatme-62d79.firebasestorage.app",
-  messagingSenderId: "962084871112",
-  appId: "1:962084871112:web:dd541e2b242c7d8ec3befd",
-};
+// Create the navigator
+const Stack = createStackNavigator();
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
 
 const App = () => {
   const connectionStatus = useNetInfo();
+  
+  // Firebase config
+  const firebaseConfig = {
+    apiKey: "AIzaSyCVw3Pwp3eETBRHUFpdSnuE9g-F0D7oTg0",
+    authDomain: "chatme-62d79.firebaseapp.com",
+    projectId: "chatme-62d79",
+    storageBucket: "chatme-62d79.firebasestorage.app",
+    messagingSenderId: "962084871112",
+    appId: "1:962084871112:web:dd541e2b242c7d8ec3befd",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const storage = getStorage(app);
 
   useEffect(() => {
     if (connectionStatus.isConnected === false) {
@@ -50,7 +51,14 @@ const App = () => {
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="Chat">
-        {props => <Chat isConnected={connectionStatus.isConnected} db={db} {...props} />}
+          {(props) => (
+            <Chat
+              isConnected={connectionStatus.isConnected}
+              storage={storage}
+              db={db}
+              {...props}
+            />
+          )}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
--- components/Chat.js
+++ components/Chat.js
@@ -1,34 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
+import { View, StyleSheet, KeyboardAvoidingView, Platform, } from "react-native";
 import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import { collection, addDoc, onSnapshot, query, orderBy } from "firebase/firestore";
+import CustomActions from './CustomActions';
 
 // import storage
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation, db, isConnected }) => {
+// import map
+import MapView from "react-native-maps";
+
+const Chat = ({ route, navigation, db, storage, isConnected }) => {
   const { name, backgroundColor, userID } = route.params;
   const [messages, setMessages] = useState([]);
-
-  // Display the name in the navigation bar
-  useEffect(() => {
-    navigation.setOptions({ title: `Welcome, ${name}!` });
-  }, [navigation, name]);
 
   // Query messages to database
   let unsubMessages;
 
+  // Display the name in the navigation bar
   useEffect(() => {
+    navigation.setOptions({ title: `Welcome, ${name}!` });
+
     if (isConnected === true) {
-      // unregister current onSnapshot() listener to avoid registering multiple listeners when
-      // useEffect code is re-executed.
       if (unsubMessages) unsubMessages();
       unsubMessages == null;
 
       const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
       unsubMessages = onSnapshot(q, (documentsSnapshot) => {
         let newMessages = [];
-        documentsSnapshot.forEach(doc => {
+        documentsSnapshot.forEach((doc) => {
           newMessages.push({
             id: doc.id,
             ...doc.data(),
@@ -44,48 +44,74 @@ const Chat = ({ route, navigation, db, isConnected }) => {
     return () => {
       if (unsubMessages) unsubMessages();
     };
-  }, [isConnected]); 
+  }, [isConnected]);
 
+  const loadCachedMessages = async () => {
+    const cachedMessages = (await AsyncStorage.getItem("messages")) || [];
+    setMessages(JSON.parse(cachedMessages));
+  };
+  
   const cacheMessages = async (messagesToCache) => {
     try {
-      await AsyncStorage.setItem('messages', JSON.stringify(messagesToCache));
+      await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
     } catch (error) {
       console.log(error.message);
     }
-  }
-
-  const loadCachedMessages = async () => {
-    const cachedMessages = await AsyncStorage.getItem("messages") || [];
-    setMessages(JSON.parse(cachedMessages));
-  }
+  };
 
   // Button to send
   const onSend = (newMessages) => {
-    addDoc(collection(db, "messages"), newMessages[0])
-  }
+    addDoc(collection(db, "messages"), newMessages[0]);
+  };
 
   // Chat Bubble
   const renderBubble = (props) => {
-    return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          right: {
-            backgroundColor: "#000",
-          },
-          left: {
-            backgroundColor: "#FFF",
-          },
-        }}
-      />
-    );
-  };
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#FFF"
+        }
+      }}
+    />
+  }
 
-   // Hide InputToolbar to prevent users to send messages while offline
-   const renderInputToolbar = (props) => {
+  // Hide InputToolbar to prevent users to send messages while offline
+  const renderInputToolbar = (props) => {
     if (isConnected) return <InputToolbar {...props} />;
     else return null;
-   }
+  };
+
+  // Send a picture or location
+  const renderCustomActions = (props) => {
+    return <CustomActions userID={userID} storage={storage} {...props} />;
+  };
+
+  const renderCustomView = (props) => {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{
+            width: 150,
+            height: 100,
+            borderRadius: 13,
+            margin: 3
+          }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
+  }
 
   return (
     // Pass the color background
@@ -94,21 +120,23 @@ const Chat = ({ route, navigation, db, isConnected }) => {
       accessibilityLabel="Chat screen"
     >
       <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={{ flex: 1 }}
-    >
-      <GiftedChat
-        messages={messages}
-        renderBubble={renderBubble}
-        renderInputToolbar={renderInputToolbar}
-        onSend={(messages) => onSend(messages)}
-        user={{
-          _id: userID,
-          name: name,
-        }}
-        accessibilityLabel="Chat interface"
-        keyboardShouldPersistTaps="handled"
-      />
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <GiftedChat
+          messages={messages}
+          renderBubble={renderBubble}
+          renderInputToolbar={renderInputToolbar}
+          onSend={(messages) => onSend(messages)}
+          renderActions={renderCustomActions}
+          renderCustomView={renderCustomView}
+          user={{
+            _id: userID,
+            name: name,
+          }}
+          accessibilityLabel="Chat interface"
+          keyboardShouldPersistTaps="handled"
+        />
       </KeyboardAvoidingView>
     </View>
   );
